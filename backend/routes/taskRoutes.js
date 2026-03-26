5e82d0308ba2b0b17d0f4cafa4b033948282c963
Sumit Kumar <tbeast935@gmail.com>
2026-03-27 00:01:43 +0530

    crud

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const {addTask,getTasks} = require("../controllers/taskController");
+const {addTask,getTasks,updateTask,deleteTask} = require("../controllers/taskController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const userRouter = express.Router();
 
 userRouter.get("/", authMiddleware, getTasks);
-userRouter.post("/addtask", authMiddleware, addTask);
-// userRouter.put("/:id", authMiddleware, updateTask);
-// userRouter.delete("/:id", authMiddleware, deleteTask);
+userRouter.post("/addTask", authMiddleware, addTask);
+userRouter.put("/updateTask/:id", authMiddleware, updateTask);
+userRouter.delete("/deletetask/:id", authMiddleware, deleteTask);
 
 module.exports = userRouter;
