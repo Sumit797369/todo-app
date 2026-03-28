7b6bf85e99b8f39e6fd7232646daa8dd3d36afb2
Sumit Kumar <tbeast935@gmail.com>
2026-03-28 22:59:43 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 app.use(express.json());
-app.use(cors())
+
+app.use(cors({
+  origin: "https://todo-app-green-alpha-38.vercel.app",
+  credentials: true
+}));
 
 app.use('/api/user',userRouter);
 app.use("/api/tasks", taskRouter);
